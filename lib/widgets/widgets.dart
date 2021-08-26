3ad8b9f2af45daa9e7eb516afadf8d3f4784b80e
Almas <saparovalams0@gmail.com>
2021-08-26 14:44:35 +0500

    First commit

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,11 @@
+export 'package:plant_disease_app/widgets/menuItem.dart';
+export 'package:plant_disease_app/widgets/menuItemCategory.dart';
+export 'package:plant_disease_app/widgets/themeToggleSwitch.dart';
+export 'package:plant_disease_app/widgets/bottomNavigation.dart';
+export 'package:plant_disease_app/widgets/checkMark.dart';
+export 'package:plant_disease_app/widgets/customTabBar.dart';
+export 'package:plant_disease_app/widgets/sortMenu.dart';
+export 'package:plant_disease_app/widgets/moreMenu.dart';
+export 'package:plant_disease_app/widgets/plantList.dart';
+export 'package:plant_disease_app/widgets/searchBar.dart';
+export 'package:plant_disease_app/widgets/buttons.dart';
